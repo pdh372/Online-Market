ddb10c88298a8aaa9cd82948092568bce6b31a4d
pdh <phamduchuy372000h@gmail.com>
2022-01-05 22:19:07 +0700

    xxx -> Footer

--- src/components/shared/containerPage/ContainerPage.jsx
+++ src/components/shared/containerPage/ContainerPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../header/Header';
-import Footer from '../footer/xxx';
+import Footer from '../footer/Footer';
 
 const ContainerPage = ({ children }) => {
 	return (
