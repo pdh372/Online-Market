ea474f746edc5590ea00373f5fd8d2313c7cf9d8
pdh <phamduchuy372000h@gmail.com>
2022-01-16 22:03:50 +0700

    cart page

--- src/pages/store/profile/modules/menu.jsx
+++ src/pages/store/profile/modules/menu.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 function Account ({ onChangeTab }) {
 	// redux
 	// handleEven
-	const handleLogout = () => {
-		console.log('logout');
-	};
+	// const handleLogout = () => {
+	// 	console.log('logout');
+	// };
 
 	return (
 		<section className='dashboard__menu'>
 			<div className='menu-square'>
 				<div className='menu-square__avt'>{/* <span className='menu-square__avt-icon'>C</span> */}</div>
-				<div className='menu-square__wb'>{'account'}</div>
+				{/* <div className='menu-square__wb'>{'account'}</div> */}
 			</div>
 
 			<div className='menu-item menu-item-actived' onClick={e => onChangeTab(0, e)}>
-				Account Information
+				Thông Tin Cửa Hàng
 			</div>
-			<div className='menu-item' onClick={handleLogout}>
-				Sign Out
-			</div>
+			{/* <div className='menu-item' onClick={handleLogout}>
+				Đăng Xuất
+			</div> */}
 		</section>
 	);
 }
