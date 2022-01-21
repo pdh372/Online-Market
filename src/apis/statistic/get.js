c2fe4fdff12a3ab97089dd4febf26ca4c8e58fe2
pdh <phamduchuy372000h@gmail.com>
2022-01-21 22:53:59 +0700

    ProductStatistics

--- src/apis/statistic/get.js
+++ src/apis/statistic/get.js
@@ -4,6 +4,10 @@ class Get {
 	user = () => {
 		return rootApi.get('statisticals/users');
 	};
+
+	product = () => {
+		return rootApi.get('statisticals/products');
+	};
 }
 
 export default new Get();
