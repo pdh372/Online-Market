e14a972fad2f0bc45b00f5e5432040e2569f3939
pdh <phamduchuy372000h@gmail.com>
2022-01-22 10:37:30 +0700

    lich su giao hang cua shipper

--- src/apis/order/get.js
+++ src/apis/order/get.js
@@ -4,6 +4,11 @@ class Get {
 	orderByStatusAndUserId = (status, userId) => {
 		return rootApi.get('/orders/filterByStatusAndUserId', { params: { status, userId } });
 	};
+
+	orderByShipper = shipperId => {
+		let id = shipperId ? shipperId : '61e6c63996db5613e2addf31';
+		return rootApi.get('/orders/shipper/' + id);
+	};
 }
 
 export default new Get();
