2aa898350a13fedc75006fd9020204330fa52fc7
pdh <phamduchuy372000h@gmail.com>
2022-01-23 22:27:35 +0700

    cong quan li giay to

--- src/apis/product/post.js
+++ src/apis/product/post.js
@@ -1,14 +1,18 @@
-import axiosJava from '../rootJava'
+import axiosJava from '../rootJava';
+import axiosDotNet from '../rootDotNet';
 
 class Post {
-  addProduct = (data) => {
-    return axiosJava.post('products', data, {
-      headers : {
+	addProduct = data => {
+		return axiosJava.post('products', data, {
+			headers : {
 				'x-auth-token' : 'token',
-      },
-    });
-  }
+			},
+		});
+	};
 
+	changeOrigin = async ({ productId, image }) => {
+		return await axiosDotNet.post('product/changeProductOrigin', { image, productId });
+	};
 }
 
 export default new Post();
--- src/pages/store/profile/modules/infomation/editProfile.jsx
+++ src/pages/store/profile/modules/infomation/editProfile.jsx
@@ -9,27 +9,60 @@ const EditProfile = () => {
 	useEffect(() => {
 		const getData = async () => {
 			const data = await productApi.get.getProductByStore('61d7e6f52a36950cf2312c57');
-
+			console.log(data);
 			setProducts(data);
 		};
 
 		getData();
 	}, []);
 
+	const handleChangeImage = async (productId, e) => {
+		let file = e.target.files[0];
+
+		let reader = new FileReader();
+		reader.readAsDataURL(file);
+
+		reader.onloadend = async function () {
+			if (reader.result) {
+				console.log({ productId, image: reader.result });
+				const res = await productApi.post.changeOrigin({ productId, image: reader.result });
+
+				const index = products.findIndex(p => p._id === productId);
+				console.log(index);
+				const newProducts = [ ...products ];
+
+				newProducts[index].productOrigin = res;
+				setProducts(newProducts);
+			}
+		};
+	};
+
 	return (
 		<article>
 			<section>
-				{products.map(p => (
-					<Form name='basic' autoComplete='off' layout='vertical' style={{ marginBottom: '100px' }}>
+				{products.map((p, index) => (
+					<Form
+						key={index}
+						name='basic'
+						autoComplete='off'
+						layout='vertical'
+						style={{ marginBottom: '100px' }}
+					>
 						<Form.Item label={'Nguồn Gốc Sản Phẩm Của Sản Phẩm XXX'} name='productOrigin'>
-							<Input placeholder='Nguồn Gốc Sản Phẩm' type='file' />
+							<Input
+								placeholder='Nguồn Gốc Sản Phẩm'
+								type='file'
+								onChange={e => handleChangeImage(p._id, e)}
+							/>
 						</Form.Item>
-						{p.image && <img src={p.image} alt='img' style={{ maxWidth: '100px', minWidth: '100px' }} />}
-						<Form.Item>
+						{p.productOrigin && (
+							<img src={p.productOrigin} alt='img' style={{ maxWidth: '100px', minWidth: '100px' }} />
+						)}
+						{/* <Form.Item>
 							<button className='' type='submit'>
 								Cập Nhật
 							</button>
-						</Form.Item>
+						</Form.Item> */}
 					</Form>
 				))}
 			</section>
